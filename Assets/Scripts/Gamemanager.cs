548b3f6e9baf23ce006a4ceafb1ced03482d71b2
César Carbajo <cesares10@gmail.com>
2019-11-11 12:15:01 +0100

    Movimiento básico sin tener en cuenta tiles ni límites

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+
+    public Player P1;
+    public List<Tile> tiles;
+    public int currentTileId;
+    //public Vector2 gridSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Inputs
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            P1.Move(Player.Direction.up);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            P1.Move(Player.Direction.down);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            P1.Move(Player.Direction.right);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            P1.Move(Player.Direction.left);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Parameters
+
+    public Vector2 pos;
+    public enum Direction {up, right, left, down}
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Direction direction)
+    {
+        switch (direction)
+        {
+
+            case Direction.up:
+                pos.y++;
+                transform.Translate(0, 0, -10);
+                break;
+            case Direction.down:
+                pos.y--;
+                transform.Translate(0, 0, 10);
+                break;
+            case Direction.right:
+                pos.x++;
+                transform.Translate(-10, 0, 0);
+                break;
+            case Direction.left:
+                pos.x--;
+                transform.Translate(10, 0, 0);
+                break;
+            default:
+                Debug.Log("Error at Player.Move(): default case in switch");
+                break;
+        }
+        //trasladar al transform (debera cambiarse por animacion de salto)
+        
+    }
+}
