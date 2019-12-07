0b5be13821188a0bb79613bac40f8502955d0f1c
JuanAM8 <juanam9k@gmail.com>
2019-12-07 23:59:12 +0100

    Música y sonido definitivos

---
+++ Assets/Scripts/SoundRecruiter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRecruiter : MonoBehaviour
+{
+    AudioManager audioManager;
+
+    public void PlaySound(int i)
+    {
+        if (GameObject.Find("Audiomanager") != null)
+        {
+            audioManager = GameObject.Find("Audiomanager").GetComponent<AudioManager>();
+            audioManager.SelectSound(i);
+            audioManager.PlaySound();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
