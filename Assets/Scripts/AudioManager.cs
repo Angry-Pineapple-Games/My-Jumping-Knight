a3e19943a24686ffc92be7156730c1afbc47964c
JuanAM8 <juanam9k@gmail.com>
2019-12-07 14:31:31 +0100

    Script AudioManager creado

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public List<AudioClip> songs;
+    public List<AudioClip> sounds;
+    private AudioSource musicSource;
+    private AudioSource soundSource;
+    private static bool created = false;
+    private String currentScene;
+
+    /*Asigna la cancion indicada a la fuente de audio*/
+    public void SelectSong(int i)
+    {
+        if (i < songs.Count) { musicSource.clip = songs[i]; }
+    }
+
+    /*Asigna el sonido indicado a la fuente de audio*/
+    public void SelectSound(int i)
+    {
+        if (i < sounds.Count) { soundSource.clip = sounds[i]; }
+    }
+
+    /*Reproduce la cancion de la fuente de audio*/
+    public void PlaySong()
+    {
+        musicSource.Play();
+    }
+
+    /*Reproduce el sonido de la fuente de audio*/
+    public void PlaySound()
+    {
+        soundSource.Play();
+    }
+
+    private void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+        }
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    private void Start()
+    {
+        /*Setup starting music*/
+        switch (currentScene)
+        {
+            case "TitleScene":
+                print("Entro");
+                musicSource.clip = songs[0];
+                PlaySong();
+                break;
+            default:
+                break;
+        }
+        
+    }
+}
+
