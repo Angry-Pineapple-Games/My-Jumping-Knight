4a1eb62ec7736ad423c85159f127c4eab4284bd5
César Carbajo <cesares10@gmail.com>
2019-12-07 18:17:23 +0100

    Tutorial implementado y texto adaptativo al tamaño de los botones

--- Assets/Scripts/General/Language.cs
+++ Assets/Scripts/General/Language.cs
@@ -11,7 +11,7 @@ public class Language : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(this);
-        language = PlayerPrefs.GetString("lang","en");
+        language = PlayerPrefs.GetString("lang", "en");
         InitDictionaries();
     }
 
@@ -27,8 +27,8 @@ public class Language : MonoBehaviour
 
     public string GetString(string key)
     {
-        if(language.Equals("es")) { return Spanish[key]; }
-        else if(language.Equals("en")) { return English[key]; }
+        if (language.Equals("es")) { return Spanish[key]; }
+        else if (language.Equals("en")) { return English[key]; }
         else { return null; }
     }
 
@@ -36,13 +36,13 @@ public class Language : MonoBehaviour
     {
         #region English
         #region Access
-        English.Add("requirement", "Only numbers and letters without accents,\nno spaces in username case");
+        English.Add("requirement", "Numbers and letters without accents only,\nno blank spaces for the username");
         English.Add("username", "Username");
         English.Add("password", "Password");
         English.Add("passwordcheck", "Confirm password");
         English.Add("login", "Login");
         English.Add("signup", "Sign up");
-        English.Add("back", "Back");
+        English.Add("back", "Go back");
         #endregion
 
         #region Menu
@@ -53,20 +53,37 @@ public class Language : MonoBehaviour
         English.Add("level2", "Level 2");
         English.Add("level3", "Level 3");
         English.Add("mode", "Mode selection");
-        English.Add("singleplayer", "Singleplayer");
+        English.Add("singleplayer", "Single player");
         English.Add("multiplayer", "Multiplayer");
         English.Add("menu", "Menu");
-        English.Add("manual", "Manual-play");
-        English.Add("auto", "Auto-play");
+        English.Add("manual", "Manual play");
+        English.Add("auto", "Autoplay");
         #endregion
 
         #region Game
         English.Add("again", "Play again");
+        English.Add("rank", "RANK:");
+        English.Add("victory", "VICTORY");
         #endregion
 
         #region General
         English.Add("maintenance", "Server in maintenance,\nsorry for the inconvenience");
         English.Add("ok", "Ok");
+        #endregion
+
+        #region Tutorial
+        English.Add("tutMovMobile", "Touch one of the spheres surounding the knight to move him the way you want");
+        English.Add("tutMovPC", "Use WASD/directional arrows or click on the spheres with the mouse to move the knight the way you want");
+        English.Add("tutMovAlert", "Pressing very fast in one direction is not appropiate. Try to follow the knight's pace");
+        English.Add("tutSpikes", "Spikes get triggered when you walk over them. Be fast!");
+        English.Add("tutSaw", "The saw always goes from one side to the other. Try to avoid it");
+        English.Add("tutArrows", "Wait for the best moment to avoid the arrows");
+        English.Add("tutBlade", "Watch out for the razor blade! It's really fast!");
+        English.Add("tutHeart", "Get a heart to recover one life");
+        English.Add("tutShield", "Grab the shield to prevent the next damage you would get");
+        English.Add("tutClock", "The hourglass makes traps go slower for a while. Take your chance!");
+        English.Add("tutFall", "If you move until the end of a path, you will fall. Be careful!");
+        English.Add("tutGoal", "Reach the goal to finish the level");
         #endregion
         #endregion
 
@@ -89,7 +106,7 @@ public class Language : MonoBehaviour
         Spanish.Add("level2", "Nivel 2");
         Spanish.Add("level3", "Nivel 3");
         Spanish.Add("mode", "Selección de modo");
-        Spanish.Add("singleplayer", "Un jugador");
+        Spanish.Add("singleplayer", "Individual");
         Spanish.Add("multiplayer", "Multijugador");
         Spanish.Add("menu", "Menú");
         Spanish.Add("manual", "Partida manual");
@@ -98,11 +115,28 @@ public class Language : MonoBehaviour
 
         #region Game
         Spanish.Add("again", "Jugar de nuevo");
+        Spanish.Add("rank", "RANGO:");
+        Spanish.Add("victory", "VICTORIA");
         #endregion
 
         #region General
         Spanish.Add("maintenance", "Servidor en mantenimiento\nsentimos las molestias");
         Spanish.Add("ok", "Ok");
+        #endregion
+
+        #region Tutorial
+        Spanish.Add("tutMovMobile", "Pulsa sobre las esferas alrededor del caballero para moverlo en la dirección que escojas");
+        Spanish.Add("tutMovPC", "Utiliza WASD, las teclas direccionales o pulsa con el ratón las esferas para mover al caballero en la dirección que escojas");
+        Spanish.Add("tutMovAlert", "Pulsar muy rápido en una dirección no es lo apropiado, intenta seguir el ritmo del caballero");
+        Spanish.Add("tutSpikes", "Los pinchos se accionan cuando pasas por encima de ellos, ¡pasa rápido!");
+        Spanish.Add("tutSaw", "La sierra siempre se mueve de un lado a otro, intenta evitarla");
+        Spanish.Add("tutArrows", "Espera al momento oportuno y podrás esquivar las flechas");
+        Spanish.Add("tutBlade", "¡Cuidado con la cuchilla giratoria! ¡Es muy rápida!");
+        Spanish.Add("tutHeart", "Obtén un corazón para recuperar una vida");
+        Spanish.Add("tutShield", "Coge el escudo para prevenir el próximo daño");
+        Spanish.Add("tutClock", "El reloj de arena hará que todas las trampas funcionen más despacio durante un tiempo, ¡aprovecha tu oportunidad!");
+        Spanish.Add("tutFall", "Si te pasas con el movimiento, caerás al vacío, ¡ve con cuidado!");
+        Spanish.Add("tutGoal", "Llega a la meta para finalizar el nivel");
         #endregion
         #endregion
     }
